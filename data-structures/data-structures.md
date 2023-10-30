007c9122e276711ced718df899d25378033b4d7f
Christian Dean <christiandeandemesa@gmail.com>
2023-10-30 12:43:52 -0700

    Linked List

--- data-structures/data-structures.md
+++ data-structures/data-structures.md
@@ -4,3 +4,11 @@
 - Homogeneous: All the elements should be of the same data type.
 - Ordered: Elements are stored and can be accessed in sequence.
 - Fixed/Static can hold a maximum number of elements, and Dynamic can hold any number of elements (e.g. JavaScript, Python, and C#).
+
+# Linked List
+
+- Used to store and manage collections of dynamic elements.
+- Dynamic: Can hold any number of elements.
+- Efficient Insertions & Deletions: Can be performed without the element shifting that is needed for arrays.
+- Inefficient Random Access: To get to a specific node you need to traverse every node in order, making it harder than accessing any element in an array.
+- No Contiguous Memory: Each node is its own block of memory unlike elements in an array where the array is the block of memory.
---
+++ data-structures/linked-list.ts
@@ -0,0 +1,157 @@
+class Node<T> {
+  data: T;
+  next: Node<T> | null;
+
+  constructor(data: T) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList<T> {
+  head: Node<T> | null;
+  size: number;
+
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  // Gets the length of the linked list.
+  length() {
+    console.log(this.size);
+  }
+
+  // Adds a node to the front of the linked list.
+  prepend(data: T): void {
+    const newNode = new Node(data);
+    newNode.next = this.head;
+    this.head = newNode;
+
+    this.size++;
+
+    console.log(`Prepended ${data} to the linked list`);
+  }
+
+  // Adds a node to the end of the linked list.
+  append(data: T) {
+    const newNode = new Node(data);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+
+      while (current.next) {
+        current = current.next;
+      }
+
+      current.next = newNode;
+    }
+
+    this.size++;
+
+    console.log(`Appended ${data} to the linked list`);
+  }
+
+  // Adds a node to a specific position in the linked list.
+  insert(data: T, position: number): void {
+    if (position < 0 || position > this.size) {
+      console.log(`Choose a position within 0 and ${this.size}`);
+      return;
+    }
+
+    const newNode = new Node(data);
+
+    if (position === 0) {
+      newNode.next = this.head;
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      let index = 0;
+
+      while (index < position - 1) {
+        current = current.next;
+        index++;
+      }
+
+      newNode.next = current.next;
+      current.next = newNode;
+    }
+
+    this.size++;
+
+    console.log(`Inserted ${data} at position ${position}`);
+  }
+
+  // Deletes a node at a specific position in the linked list.
+  delete(position: number): void {
+    if (position < 0 || position > this.size) {
+      console.log(
+        `There is nothing to delete because the linked list is empty, or the position chosen is greater than ${this.size}`
+      );
+      return;
+    }
+
+    if (position === 0) {
+      if (this.head) {
+        this.head = this.head.next;
+        this.size--;
+
+        console.log(`Deleted node at position ${position}`);
+      } else {
+        console.log(
+          "There is nothing to delete because the linked list is empty"
+        );
+      }
+      return;
+    }
+
+    let current = this.head;
+    let previous: Node<T> | null = null;
+    let index = 0;
+
+    while (current && index < position) {
+      previous = current;
+      current = current.next;
+      index++;
+    }
+
+    if (current) {
+      if (previous) {
+        previous.next = current.next;
+      }
+    }
+
+    this.size--;
+
+    console.log(`Deleted node at position ${position}`);
+  }
+
+  // Gets a node at a specific position in the linked list.
+  get(position: number) {
+    if (position < 0 || position > this.size) {
+      console.log(`Choose a position within 0 and ${this.size}`);
+      return;
+    }
+
+    let current = this.head;
+    let index = 0;
+
+    while (current && index < position) {
+      current = current.next;
+      index++;
+    }
+
+    console.log(current.data);
+  }
+}
+
+const linkedList = new LinkedList<string>();
+
+linkedList.prepend("A"); // Prepended A to the linked list
+linkedList.append("C"); // Appended C to the linked list
+linkedList.insert("B", 1); // Inserted B at position 1
+linkedList.length(); // 3
+linkedList.delete(0); // Deleted node at position 0
+linkedList.get(0); // B
